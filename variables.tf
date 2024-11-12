249664aa3fb042a62e1bb026a9a916049dc537bb
Sahithchaganti2004 <132831862+Sahithchaganti2004@users.noreply.github.com>
2024-11-12 22:02:25 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -5,3 +5,8 @@ variable "image_tag" {
   description = "The Docker image tag"
   type        = string
 }
+variable "app_version" {
+  description = "Application version tag for Docker image"
+  type        = string
+  default     = "1.0.0"  # Update only with significant changes
+}
